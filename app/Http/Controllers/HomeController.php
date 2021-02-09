5ccbab3860e25998794620bed616d71f082da4b4
Giuseppe <giuseppe.dagostino.d@gmail.com>
2021-02-09 18:28:42 +0100

    Prima query
    1 - Modifica del file php.ini
    2 - Rilancio MAMP e php artisan serve
    3 - Rilancio composer dump-autoload
    4 - Scrivo la prima query e stampo tutto con un dd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Car;
 
 class HomeController extends Controller
 {
     public function index() {
+        // prima query - select * from movies
+        // accedo in maniera statica al suo metodo statico
+        // public static function all
+        $cars = Car::all();
+        dd($cars);
+
         return view('home');
     }
 }
